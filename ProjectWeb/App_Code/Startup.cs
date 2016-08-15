9f4b428148a9c12b5f62d04e693c33bc794eb2b5
Dragon Of Justice <theguncrusher@gmail.com>
2016-08-15 18:57:17 +0700

    Add project files.

---
+++ ProjectWeb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectWeb.Startup))]
+namespace ProjectWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
